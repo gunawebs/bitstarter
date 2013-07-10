03de79ac5b3b6804b8aff3f08cc301928fa76e0a
Gunaranjan Pemmaraju <guna.webs@gmail.com>
2013-07-10 17:35:00 +0000

    modified web.js

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
 
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
+var buffer = fs.readFileSync('index.html');
+var content = buffer.toString();
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
