d14f2ae78f752ba656119ca17b50f0f5121cc6cf
gunawebs <guna.webs@gmail.com>
2013-08-19 19:09:16 +0530

    Adding Native CUrrency

---
+++ script/ftscript.js
@@ -0,0 +1,23 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: guna
+ * Date: 8/19/13
+ * Time: 7:02 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-43032473-1']);
+_gaq.push(['_trackPageview']);
+
+(function () {
+    var ga = document.createElement('script');
+    ga.type = 'text/javascript';
+    ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ga, s);
+    })();
+
+
