41e7d5629e4e5ffacaaec74f6a73cef236ae99a2
gunawebs <guna.webs@gmail.com>
2013-08-19 19:39:59 +0530

    Adding Native CUrrency

--- web.js
+++ web.js
@@ -12,8 +12,8 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.set('port', process.env.PORT || 8080);
 
-app.use(express.static(__dirname + '/static'));
-
+app.use("/css", express.static(__dirname + '/css'));
+app.use("/script", express.static(__dirname + '/script'));
 
 // Render homepage (note trailing slash): example.com/
 app.get('/', function(request, response) {
